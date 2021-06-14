e792c4a1e5e4d231e643b0682ca41a11228e9feb
rosca bogdan <bogdan.rosca@rbpixel.com>
2021-06-14 19:50:04 +0300

    live coding examples

--- wp-content/themes/wooiap/functions.php
+++ wp-content/themes/wooiap/functions.php
@@ -48,3 +48,67 @@ function iap_woocommerce_functions() {
 	// add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_title', 5 );
 }
 
+
+/**
+ * Adauga un prag minim pentru comanda.
+ * */
+add_action( 'woocommerce_check_cart_items', 'iap_required_min_cart_subtotal_amount' );
+function iap_required_min_cart_subtotal_amount() {
+
+	// setam pragul minim
+	$minimum_amount = 50;
+
+	// Sub-total (inainte de taxe si taxe de expediere)
+	$cart_subtotal = WC()->cart->subtotal;
+
+	// Cat mai avem de cumparat 
+	$money_to_spend = $minimum_amount - $cart_subtotal;
+
+	// Adauga o notificare in cos cumparaturi daca nu este indeplinita conditia.
+	if( $cart_subtotal < $minimum_amount  ) {
+
+		// Afiseaza mesaj de eroare
+		wc_add_notice( '' . sprintf( __("Comanda minima este de <strong>%1s</strong>. Pentru a putea finaliza comanda trebuie sa mai adaugati in cos produse in valoare de <strong>%2s</strong>."),wc_price($minimum_amount), wc_price($money_to_spend) ) . '', 'error' );
+
+		// Optional: sterge butonul de checkout
+		remove_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20 );
+	}	
+}
+
+
+/**
+ * Ascunde celelate metode de livrare daca freeshipping este activ.
+ * */
+function iap_hide_shipping_when_free_is_available( $rates ) {
+	$free = array();
+	foreach ( $rates as $rate_id => $rate ) {
+		if ( 'free_shipping' === $rate->method_id ) {
+			$free[ $rate_id ] = $rate;
+			break;
+		}
+	}
+	return ! empty( $free ) ? $free : $rates;
+}
+add_filter( 'woocommerce_package_rates', 'iap_hide_shipping_when_free_is_available', 100 );
+
+function iap_show_custom_product_message()  {
+	
+	$product_id = get_the_ID();
+	$categorii = get_the_terms( $product_id, 'product_cat' );
+	// var_dump($categorii);
+	foreach ($categorii as $categorie) {
+		if ( $categorie->term_id == '16' ) : 
+			echo '<div class="woocommerce-notices-wrapper"><div class="woocommerce-message" role="alert">
+			Acest produs face parte din gama produselor <strong>ECO</strong>.	</div></div>';  
+		endif;
+	}
+
+	// if ( $product_id == '14' ) :
+	// 	echo '<div class="woocommerce-notices-wrapper"><div class="woocommerce-message" role="alert">
+	// 	Acest produs face parte din gama produselor <strong>ECO</strong>.	</div></div>';
+	// else: 
+	// 	// nu afisa nimic
+	// endif;
+};
+
+add_action( 'woocommerce_single_product_summary', 'iap_show_custom_product_message', $priority = 20 );
